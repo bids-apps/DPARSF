61b2936bbf1f53dc09701e8be19b4155ae4b6bec
Chaogan-Yan <ycg.yan@gmail.com>
2017-11-27 11:57:31 +0800

    Fixed TRInfo.tsv error

--- run.sh
+++ run.sh
@@ -6,5 +6,7 @@ optvalues="$@"
 
 octave --silent --eval "y_Convert_BIDS2DPARSFA(\"$1\",\"$2\",\"${optvalues}\")"
 
+rm -rf $2/TRInfo.tsv
+
 ${DPARSFPath}/run_DPARSFA_run.sh ${MCRPath} $2/DPARSFACfg.mat $2 $2/SubID.txt 0
 
